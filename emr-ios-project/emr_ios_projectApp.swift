7a93c5a936714e06f2a2e4d96e4bcb548d54324e
Malay Joshi <malayace.joshi@gmail.com>
2022-04-28 14:54:17 -0400

    Initial Commit

---
+++ emr-ios-project/emr_ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  emr_ios_projectApp.swift
+//  emr-ios-project
+//
+//  Created by Malay Joshi on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct emr_ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
